1ca1e7f4da02d82f0e0a45e679ba9691b2fff471
Sunil Shelke <shelkesunil072@gmail.com>
2026-04-17 18:18:27 +0530

    new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import "./App.scss"
+
 const App = () => {
   return (
     <div>
       <h1>Welcome to the Real Estate App</h1>
+     
     </div>
   )
 }
