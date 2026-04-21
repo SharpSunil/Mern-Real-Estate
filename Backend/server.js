5aa7cbbc47d9ee63284bf6a52049afd537904b53
Sunil Shelke <shelkesunil072@gmail.com>
2026-04-21 17:22:35 +0530

    nrew

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import http from "http";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Databse connection 
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+//Routes 
+app.get('/', (req, res) => {
+    res.send("API is running...");
+})
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port :${PORT}`);
+})
