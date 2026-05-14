9577fdc13d8c3d935fed8ec09a5781c48f49f733
Sunil Shelke <shelkesunil072@gmail.com>
2026-05-14 18:02:51 +0530

    new

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://shelkesunil072_db_user:03GV3mUKk111anhD@cluster0.b7kthqn.mongodb.net/RealState"
+      "mongodb://shelkesunil072_db_user:03GV3mUKk111anhD@ac-16vmiwx-shard-00-00.b7kthqn.mongodb.net:27017,ac-16vmiwx-shard-00-01.b7kthqn.mongodb.net:27017,ac-16vmiwx-shard-00-02.b7kthqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-eua1c0-shard-0&authSource=admin&appName=Cluster0"
     )
     .then(() => {
       console.log("DB Connected");
