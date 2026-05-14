515120c21e02df8366f07c1fcaa14ae117f9d9f1
Sunil Shelke <shelkesunil072@gmail.com>
2026-05-14 18:06:22 +0530

    new

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb://shelkesunil072_db_user:03GV3mUKk111anhD@ac-16vmiwx-shard-00-00.b7kthqn.mongodb.net:27017,ac-16vmiwx-shard-00-01.b7kthqn.mongodb.net:27017,ac-16vmiwx-shard-00-02.b7kthqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-eua1c0-shard-0&authSource=admin&appName=Cluster0"
-    )
+    .connect(process.env.MONGODB_URI)
     .then(() => {
       console.log("DB Connected");
     })
