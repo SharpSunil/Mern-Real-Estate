fa94539542366a4570146d6babe217c57ee56fa4
Sunil Shelke <shelkesunil072@gmail.com>
2026-06-09 22:02:20 +0530

    new

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-parent">
+        <div className="footer-cont cont">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
