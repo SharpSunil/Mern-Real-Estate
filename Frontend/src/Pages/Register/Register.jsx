b300ae5704a4c67c4c1af1a9753e45eb56f5b332
Sunil Shelke <shelkesunil072@gmail.com>
2026-06-19 17:55:24 +0530

    new

--- Frontend/src/Pages/Register/Register.jsx
+++ Frontend/src/Pages/Register/Register.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import './register.scss'
 
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [showPassword, setShowPassword] = useState(false);
+    const [role, setRole] = useState("Buyer");
+    return (
+        <>
+            <div className="register-parent parent">
+                <div className="register-cont cont">
+                    <div className="form-box">
+                        <div className="heading">Create Account</div>
+                        <p>Join our community to find or list properties</p>
 
-export default Register
+                        <form>
+                            <label>Full Name</label>
+                            <input
+                                type="text"
+                                placeholder="Enter Your Full Name"
+                            />
+                            <label>Email Address</label>
+                            <input
+                                type="email"
+                                placeholder="enteryouremail@gmail.com"
+                            />
+
+                            <div className="second-box">
+                                <label>Password</label>
+
+                                <div className="password-box">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder=". . . . ."
+                                    />
+
+                                    <span
+                                        className="eye-icon"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                    >
+                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                    </span>
+                                </div>
+
+
+                            </div>
+                            <div className="second-box">
+                                <label>Select Role</label>
+
+                                <div className="role-box">
+                                    <div
+                                        className={`role-btn ${role === "Buyer" ? "active" : ""}`}
+                                        onClick={() => setRole("Buyer")}
+                                    >
+                                        Buyer
+                                    </div>
+
+                                    <div
+                                        className={`role-btn ${role === "Seller" ? "active" : ""}`}
+                                        onClick={() => setRole("Seller")}
+                                    >
+                                        Seller
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button className="btn">Create Account</button>
+
+                            <p>
+                                Already have an account?{" "}
+                                <Link to="/Login">
+                                    Sign In
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Register;
